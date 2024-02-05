06df8eb4866d955de8087f76fd5452c0bb9eb6d8
Ivan Erofeev <ivan.erofeev.07@mail.ru>
2024-02-05 13:21:25 +0300

    v2

--- README.md
+++ README.md
@@ -8,9 +8,14 @@
 
 Mindustry plugin to auto pause empty server.
 
+Ideas from [pisceskkk/Mindustry_ServerAutoPause](https://github.com/pisceskkk/Mindustry_ServerAutoPause?ysclid=ls8s5qtps5461564390)
+
 ## Features
 
 - When the last player leave the server, the game will be paused automatically.
+- When the server host a new game with no player, the game will be paused automatically.
+- When the first player connect to the paused server, the game will be resumed automatically.
+- Command `autopause <on/off>` for server and client to control if the server pause automatically.
 
 ## Downloading
 
--- src/AutoPause/AutoPausePlugin.java
+++ src/AutoPause/AutoPausePlugin.java
@@ -1,17 +1,23 @@
 package AutoPause;
 
 import arc.Events;
+import arc.util.CommandHandler;
+import arc.util.Log;
 import mindustry.Vars;
 import mindustry.core.GameState;
+import mindustry.gen.Player;
 import mindustry.mod.Plugin;
 import mindustry.game.EventType;
 
 public class AutoPausePlugin extends Plugin {
-    private final boolean enabled = true;
+    private boolean enabled = true;
     private int player_count = 0;
 
     @Override
     public void init() {
+        Log.info("[I][AutoPause] AutoPause initialization started!!");
+        Vars.state.set(GameState.State.paused);
+
         Events.on(EventType.WorldLoadEndEvent.class, event -> {
             if (enabled) {
                 Vars.state.set(GameState.State.paused);
@@ -33,5 +39,63 @@ public class AutoPausePlugin extends Plugin {
                 }
             }
         });
+
+        Log.info("[I][AutoPause] AutoPause initialization completed!!");
+    }
+
+    @Override
+    public void registerServerCommands(CommandHandler handler) {
+        handler.register(
+                "autopause",
+                "<on/off>",
+                "Enabled/disable autopause", arg ->{
+            if (arg.length == 0) {
+                Log.info("[AutoPause] Error: Second parameter must be either 'on' or 'off'");
+            }
+
+            if (!(arg[0].equals("on") || arg[0].equals("off"))) {
+                Log.info("[AutoPause] Error: Second parameter must be either 'on' or 'off'");
+
+            }
+
+            if (arg[0].equals("off")) {
+                enabled = false;
+                Log.info("[I][AutoPause] AutoPause off");
+            }
+
+            if (arg[0].equals("on")) {
+                enabled = true;
+                Log.info("[I][AutoPause] AutoPause on");
+            }
+        });
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+        handler.<Player>register(
+                "autopause",
+                "<on/off>",
+                "Enabled/disable autopause", (arg, player) -> {
+                    if (arg.length == 0) {
+                        player.sendMessage("Error: Second parameter must be either 'on' or 'off'");
+                    }
+
+                    if (!(arg[0].equals("on") || arg[0].equals("off"))) {
+                        player.sendMessage("Error: Second parameter must be either 'on' or 'off'");
+
+                    }
+
+                    if (arg[0].equals("off")) {
+                        enabled = false;
+                        Log.info("[I][AutoPause] AutoPause off");
+                        player.sendMessage("AutoPause off");
+                    }
+
+                    if (arg[0].equals("on")) {
+                        enabled = true;
+                        Log.info("[I][AutoPause] AutoPause on");
+                        player.sendMessage("AutoPause on");
+                    }
+                });
     }
 }
